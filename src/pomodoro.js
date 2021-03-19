7ca9b38bf5ff5c666f79a8fcbf17bc266ab631e3
Victor Henrique <victorhrvs@gmail.com>
2021-03-18 23:43:52 -0300

    adicionado as funções aos botões do relógio

--- src/pomodoro.js
+++ src/pomodoro.js
@@ -1,28 +1,77 @@
 //import Timer from './dist/easytimer.min.js';
-var timer = new easytimer.Timer();
+
+//import Timer from './dist/easytimer.min.js';
+let timer_green = new easytimer.Timer();
+let timer_yellow = new easytimer.Timer();
+let green_init = {minutes: 25, seconds: 0}
+let yellow_init = {minutes: 0, seconds: 10}
 var clock_selected = 'clock_green';
+init();
 
-timer.start({countdown: true, startValues: {seconds: 90}});
+function init() {
+    // Inicia o relogio verde com o valor de green_init
+    let green_el = document.getElementById('clock_green');
+    timer_green.start({countdown: true, startValues: {minutes: green_init.minutes, seconds: green_init.seconds}});
+    green_el.innerHTML = timer_green.getTimeValues().toString(['minutes', 'seconds']);
+    timer_green.stop();
+}
 
-timer.addEventListener('secondsUpdated', function (e) {
-    let clock = document.getElementById(clock_selected)
-    let clock_text = timer.getTimeValues().toString(['minutes', 'seconds'])
-    //clock_green.innerHTML(timer.getTimeValues().toString());
-    console.log(timer.getTimeValues().toString(['minutes', 'seconds']) );
+timer_green.addEventListener('secondTenthsUpdated', function (e) {
+    let clock = document.getElementById(clock_selected);
+    let clock_text = timer_green.getTimeValues().toString(['minutes', 'seconds']);
+    //clock_green.innerHTML(timer_green.getTimeValues().toString());
+    console.log(timer_green.getTimeValues().toString(['minutes', 'seconds']) );
     clock.innerHTML = clock_text;
 });
 
+timer_yellow.addEventListener('secondTenthsUpdated', function (e) {
+    let clock = document.getElementById(clock_selected)
+    let clock_text = timer_yellow.getTimeValues().toString(['minutes', 'seconds'])
+    //clock_green.innerHTML(timer_green.getTimeValues().toString());
+    console.log(timer_yellow.getTimeValues().toString(['minutes', 'seconds']) );
+    clock.innerHTML = clock_text;
+});
+ 
 
 function rewind() {
     let green_el = document.getElementById('clock_green');
     let yellow_el = document.getElementById('clock_yellow');
     
-    if(green_el.style.display === "none") {
-        alert('rewind');
+    if( clock_selected === "clock_green") {
+        //Inicia relogio
+        timer_green.stop();
+        timer_green.start({countdown: true, startValues: {minutes: green_init.minutes, seconds: green_init.seconds}});
+
+        green_el.innerHTML = timer_green.getTimeValues().toString(['minutes', 'seconds']);
+        timer_green.stop();
+    }
+
+    if( clock_selected === "clock_yellow") {
+        //Inicia relogio
+        timer_yellow.stop();
+        timer_yellow.start({countdown: true, startValues: {minutes: yellow_init.minutes, seconds: yellow_init.seconds}});
+
+        yellow_el.innerHTML = timer_yellow.getTimeValues().toString(['minutes', 'seconds']);
+        timer_yellow.stop();
     }
 }
 
+function audio() {
+    var sound = document.getElementById("audio");
+    if (sound.muted == true){
+        sound.muted = false;
+    }
+    sound.play();
+}
+
 function play() {
+    console.log(clock_selected === 'clock_yellow');
+    if( clock_selected === 'clock_green'){
+        timer_green.start({countdown: true, startValues: {minutes: green_init.minutes, seconds: green_init.seconds}});
+    }
+    if( clock_selected === 'clock_yellow'){
+        timer_yellow.start({countdown: true, startValues: {minutes: yellow_init.minutes, seconds: yellow_init.seconds}});
+    }
     
 }
 
@@ -30,20 +79,22 @@ function next() {
     let green_el = document.getElementById('clock_green');
     let yellow_el = document.getElementById('clock_yellow');
     
-    //console.log(green_el)
-    //console.log(green_el.style.display === 'none')
-    //console.log(yellow_el.style.display === 'none')
+    //Green Clock Select
     if(green_el.style.display === 'none') {
         green_el.style.display = "flex";
         yellow_el.style.display = "none";
         clock_selected = 'clock_green';
-
+        timer_yellow.stop();    
     }
+    //Yellow Clock Select
     else{
             yellow_el.style.display = "flex";
             green_el.style.display = "none";
             clock_selected = 'clock_yellow';
+            timer_green.stop();
+        
     }
+    rewind();
 }
 
 function hide_menu() {
@@ -64,12 +115,17 @@ function hide_menu() {
 
 }
 
-function hide() {
+function setTime() {
+    let value_green = document.getElementById('input_clock_green');
+    let value_yellow = document.getElementById('input_clock_yellow');
     
+    console.log("Green " + value_green.value);
+    console.log("Yellow " + value_yellow.value);
+    console.log('set Time');
 }
 
 //document.getElementById('btn-play').click(function () {
-//    console.log(timer.getTimeValues().toString());
+//    console.log(timer_green.getTimeValues().toString());
 //});
 
 let bnt_rewind_el = document.querySelector('#btn-rewind');
@@ -83,6 +139,13 @@ bnt_play_el.addEventListener('click', play);
 let bnt_next_el = document.querySelector('#btn-next');
 bnt_next_el.addEventListener('click', next);
 
-let bnt_navigation_bar_el = document.querySelector('#navigation_bar');
+let i;
+let bnt_set_clock_time_el = document.getElementsByClassName('btn-barra');
+for (i = 0; i < bnt_set_clock_time_el.length; i++) {
+    bnt_set_clock_time_el[i].addEventListener('click', setTime);
+}
+
+let bnt_navigation_bar_el = document.getElementById('navigation_bar');
 bnt_navigation_bar_el.addEventListener('click', hide_menu);
+
 
